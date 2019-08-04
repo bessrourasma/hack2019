1fdc2388c9cba59e863ead92bf05cc9a9d2c58eb
bessrourasma <bessrourasma@gmail.com>
2019-08-04 09:36:14 +0200

    il jdid ya baba

--- DB.php
+++ DB.php
@@ -22,6 +22,39 @@ $cin=$_POST['cin'];
 $tel=$_POST['ph'];
 
 
-$sql="INSERT INTO personnes SET $mail , $psw , $nf , $cin , $tel;";
+$sql="INSERT INTO id SET $mail , $psw , $nf , $cin , $tel;";
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$sql1="UPDATE id 
+set $mail=$nmail" 
+if ($conn->query($sql1) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql1 . "<br>" . $conn->error;
+}
+$sql2="UPDATE id 
+set $psw=$npsw" 
+if ($conn->query($sql2) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql2 . "<br>" . $conn->error;
+}
+$sql3="UPDATE id 
+set $cin=$ncin" 
+if ($conn->query($sql3) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql3 . "<br>" . $conn->error;
+}$sql4="UPDATE id 
+set $tel=$ntel" 
+if ($conn->query($sql4) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql4 . "<br>" . $conn->error;
+}
+
 
 ?>
