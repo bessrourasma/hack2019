c74cfbbd58517b0bd7cd4eb198620eb1cf7a15bd
bessrourasma <bessrourasma@gmail.com>
2019-08-04 03:39:58 +0200

    ppp

---
+++ anim.js
@@ -0,0 +1,18 @@
+ $(document).ready(setInterval(function(){
+
+    var title = $("#titre");
+     {
+    
+      title.css("opacity", "1");
+      
+      
+        title.animate({opacity: 0}, 800);
+        title.animate({opacity: 1}, 900);   
+      
+
+    }
+   
+  }),1000);
+
+
+      
