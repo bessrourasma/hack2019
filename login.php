3ecd4b984e8bdd3180e58a05b39d2c7a026b87d3
bessrourasma <bessrourasma@gmail.com>
2019-08-04 05:32:24 +0200

    nw

--- login.php
+++ login.php
@@ -2,24 +2,24 @@
    include("config.php");
    session_start();
    
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
+   if($_SERVER["REQUEST_METHOD"] == "GET") {
+      // username and password 
       
-      $myusername = mysqli_real_escape_string($db,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+      $myusername = mysqli_real_escape_string($db,$_GET['username']);
+      $mypassword = mysqli_real_escape_string($db,$_GET['password']); 
       
-      $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+      $sql = "SELECT id FROM admin WHERE username = '$mail' and passcode = '$psw'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $active = $row['active'];
       
       $count = mysqli_num_rows($result);
       
-      // If result matched $myusername and $mypassword, table row must be 1 row
+      // if result matched $mail and $psw, table row must be 1 row
 		
       if($count == 1) {
          session_register("myusername");
-         $_SESSION['login_user'] = $myusername;
+         $_SESSION['login_user'] = $mail;
          
          header("location: welcome.php");
       }else {
