f0ffd9a7d95d9107687543e7cad7ada132f2a1ca
nouha <nahnouha.hamrounift@gmail.com>
2019-08-04 02:02:10 +0100

    animation

---
+++ anim.js
@@ -0,0 +1,24 @@
+$(document).ready(setInterval(function(){
+
+    var title = $(".lang");
+     {
+    
+      title.css("opacity", "1");
+      
+      
+        title.animate({opacity: 0}, 600);
+        title.animate({opacity: 1}, 600);   
+      
+
+    }
+   
+  }),1000);
+
+  $(document).ready(function(){
+  
+      
+      $(".tp").hide();
+      $(".tp").show(2000);
+});
+      
+   
